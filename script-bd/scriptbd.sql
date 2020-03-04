0daae78c8631ed5017b6a29cee80d1c5259f0163
Camilo Rivera <ca-milin15@hotmail.com>
2020-03-03 22:41:54 -0500

    Modificar script de base de datos
    Se modificó el script con todas las tablas.

--- script-bd/scriptbd.sql
+++ script-bd/scriptbd.sql
@@ -1,114 +1,210 @@
-create database veterinaria
-use veterinaria
+create database veterinaria_db
+use veterinaria_db
 
-create table tbl_usuario
+
+create table Usuario
 (
-id int not null,
+id int not null AUTO_INCREMENT,
 nombre varchar(50) not null,
 apellidos varchar(50) not null,
 direccion varchar(50) not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-PRIMARY KEY (id)
-);
-
-create table tbl_historiaClinica
-(
-id int not null,
-nombre varchar(50) not null,
-historia varchar(900) not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-PRIMARY KEY (id)
-);
-
-create table tbl_tarifa
-(
-id int not null,
-nombre varchar(50) not null,
-valor int not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-PRIMARY KEY (id)
-);
-
-create table tbl_raza
-(
-id int not null,
-nombre varchar(50) not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-usuarioCreacion int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (usuarioCreacion) REFERENCES tbl_usuario(id)
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
 );
 
-create table tbl_tipoServicio
+create table HistoriaClinica
 (
-id int not null,
+id int not null AUTO_INCREMENT,
 nombre varchar(50) not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-usuarioCreacion int not null,
+historia varchar(500) not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (usuarioCreacion) REFERENCES tbl_usuario(id)
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
 );
 
-create table tbl_servicio
+create table Tarifa
 (
-id int not null,
+id int not null AUTO_INCREMENT,
+nombre varchar(50) not null,
+valor float not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table Raza
+(
+id int not null AUTO_INCREMENT,
+nombre varchar(50) not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table TipoServicio
+(
+id int not null AUTO_INCREMENT,
+nombre varchar(50) not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table Servicio
+(
+id int not null AUTO_INCREMENT,
 nombre varchar(50) not null,
 codigo varchar(10) not null,
 tipoServicio int not null,
 tarifaId int not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (tarifaId) REFERENCES tbl_tarifa(id)
+FOREIGN KEY (tarifaId) REFERENCES Tarifa(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
 );
 
-create table tbl_tipoMascota
+
+create table TipoMascota
 (
-id int not null,
+id int not null AUTO_INCREMENT,
 nombre varchar(50) not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-usuarioCreacion int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (usuarioCreacion) REFERENCES tbl_usuario(id)
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
 );
 
-create table tbl_mascota
+create table Carnet
+(
+id int not null AUTO_INCREMENT,
+codigo int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id)
+);
+
+create table Producto(
+id int not null AUTO_INCREMENT,
+nombreProducto varchar(50) not null,
+PRIMARY KEY (id)
+);
+
+create table DetalleCarnet
 (
 id int not null,
+carnetId int not null,
+productoId int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (carnetId) REFERENCES Carnet(id),
+FOREIGN KEY (productoId) REFERENCES Producto(id)
+);
+
+create table Mascota
+(
+id int not null AUTO_INCREMENT,
 nombre varchar(50) not null,
 usuarioId int not null,
 edad int not null,
 fechaNacimiento date not null,
 historiaClinicaId int not null,
 razaId int not null,
+carnetId int not null,
 tipoMascotaId int not null,
-peso double not null,
-altura double not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
-usuarioCreacion int not null,
+peso float not null,
+altura float not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (usuarioId) REFERENCES tbl_usuario(id),
-FOREIGN KEY (historiaClinicaId) REFERENCES tbl_historiaClinica(id),
-FOREIGN KEY (razaId) REFERENCES tbl_raza(id),
-FOREIGN KEY (tipoMascotaId) REFERENCES tbl_tipoMascota(id),
-FOREIGN KEY (usuarioCreacion) REFERENCES tbl_usuario(id)
+FOREIGN KEY (usuarioId) REFERENCES Usuario(id),
+FOREIGN KEY (historiaClinicaId) REFERENCES HistoriaClinica(id),
+FOREIGN KEY (razaId) REFERENCES Raza(id),
+FOREIGN KEY (tipoMascotaId) REFERENCES TipoMascota(id),
+FOREIGN KEY (carnetId) REFERENCES Carnet(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
 );
 
-create table tbl_servicioMascota
+create table Cita
 (
-id int not null,
+id int not null AUTO_INCREMENT,
 servicioId int not null,
 mascotaId int not null,
-fechaCreacion date not null,
-fechaActualizacion date not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
 PRIMARY KEY (id),
-FOREIGN KEY (servicioId) REFERENCES tbl_servicio(id),
-FOREIGN KEY (mascotaId) REFERENCES tbl_mascota(id)
+FOREIGN KEY (servicioId) REFERENCES Servicio(id),
+FOREIGN KEY (mascotaId) REFERENCES Mascota(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table RecursoVeterinaria
+(
+id int not null AUTO_INCREMENT,
+codigo varchar(10) not null,
+nombre varchar(60) not null,
+usuarioId int,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (usuarioId) REFERENCES Usuario(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table RecursoVeterinariaAsignado
+(
+id int not null AUTO_INCREMENT,
+servicioId int not null,
+recursoVeterinariaId int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (servicioId) REFERENCES Servicio(id),
+FOREIGN KEY (recursoVeterinariaId) REFERENCES RecursoVeterinaria(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table Disponibilidad
+(
+id int not null AUTO_INCREMENT,
+fecha datetime not null,
+recursoVeterinariaId int not null,
+servicioId int not null,
+fechaCreacion datetime not null,
+fechaActualizacion datetime not null,
+usuarioCreacionId int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (recursoVeterinariaId) REFERENCES RecursoVeterinaria(id),
+FOREIGN KEY (servicioId) REFERENCES Servicio(id),
+FOREIGN KEY (usuarioCreacionId) REFERENCES Usuario(id)
+);
+
+create table DetalleDisponibilidad
+(
+id int not null AUTO_INCREMENT,
+disponibilidadId int not null,
+horaInicio datetime not null,
+horaFinal datetime not null,
+PRIMARY KEY (id),
+FOREIGN KEY (disponibilidadId) REFERENCES Disponibilidad(id)
 );
